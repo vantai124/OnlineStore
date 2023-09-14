822de53b268b6fed4c171e1e02129ad1dbbfb147
vantai124 <tainguyen220603@gmail.com>
2023-09-14 13:46:55 +0700

    project onlinestore

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+
+
+@extends('layouts.app')
+@section('title','home page - online store')
+@section('content')
+<div class="text-center">
+    wellcome to the applicatiom
+</div>
+    
+
+@endsection
